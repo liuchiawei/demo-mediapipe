1990cc3a6eebc2e61436dfead24d8424c354fc2d
LIU CHIAWEI <doublecheap@gmail.com>
2026-02-10 17:31:45 +0900

    refactor(HomePage): replace HandPose with HandPoseDetector, update component structure and layout; remove unused MediaPipeTasksVisionPage and HandPose components

--- app/mediapipe-tasks-vision/page.tsx
+++
@@ -1,9 +0,0 @@
-import HandPoseDetector from "@/components/HandPoseDetector";
-
-export default function MediaPipeTasksVisionPage() {
-  return (
-    <main className="flex flex-col justify-start items-center w-full max-w-3xl min-h-svh pt-6 pb-8 px-4 mx-auto">
-      <HandPoseDetector />
-    </main>
-  );
-};
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import HandPose from "../components/HandPose";
-import { TopPage } from "@/lib/message";
+import HandPoseDetector from "@/components/HandPoseDetector";
 
-export default function Home() {
+export default function MediaPipeTasksVisionPage() {
   return (
-    <main className="flex flex-col justify-center items-center w-full h-svh mx-auto">
-      <header className="space-y-1" aria-label="text-area">
-        <h2 className="text-lg font-semi-bold font-roboto text-center">{TopPage.titleEn}</h2>
-        <h1 className="text-3xl font-black text-center">{TopPage.title}</h1>
-        <p className="text-sm text-gray-500 text-justify">{TopPage.description}</p>
-      </header>
-      <section className="w-full h-full flex justify-center items-center">
-        <HandPose />
-      </section>
+    <main className="flex flex-col justify-start items-center w-full max-w-3xl min-h-svh pt-6 pb-8 px-4 mx-auto">
+      <HandPoseDetector />
     </main>
   );
 }
